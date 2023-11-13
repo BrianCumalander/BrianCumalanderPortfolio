a97582196cb0f263c6d2b59ab827fe67a0793322
BrianCumalander <koomo24@gmail.com>
2023-11-13 07:46:34 -0700

    Added pages/Weather

---
+++ src/pages/Weather.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+
+
+function Weather() {
+    return (
+        <div className="Weather">
+            
+            <Footer />
+        </div>
+    )
+}
+
+export default Weather
