98e676e78795e25e814191dd26c0f708c6323318
BrianCumalander <koomo24@gmail.com>
2023-11-22 00:45:30 -0700

    removed un-used space in Route path

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Components
 import Home from './pages/Home';
+// import Footer from './components/Footer';
+// import Intro from './components/Intro';
+// import Portfolio from './components/portfolio';
+// import Timeline from './data/timeline';
 
 
 
@@ -10,9 +14,18 @@ import Home from './pages/Home';
 const App = () => (
     <Router>
         <Routes>
-            <Route path='/'         element={<Home />} />
+            <Route path='/' element={<Home />} />
         </Routes>
     </Router>
+    //  <div className='bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter'>
+    //     <div className='max-w-5xl w-11/12 mx-auto'>
+    //         <Intro />
+    //         <Portfolio />
+    //         <Timeline />
+    //         <Contact />
+    //         <Footer />
+    //     </div>
+    // </div>
     
 );
 
