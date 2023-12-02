cea3ab5390337c89392231c504890c67975ef9a3
BrianCumalander <koomo24@gmail.com>
2023-12-02 13:55:19 -0700

    removing file.

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Components
 import Home from './pages/Home';
-// import Footer from './components/Footer';
-// import Intro from './components/Intro';
-// import Portfolio from './components/portfolio';
-// import Timeline from './data/timeline';
-
-
+import Weather from './pages/Projects/Weather';
+import NotFound from './pages/NotFound';
+import TomatoCounter from './pages/Projects/TomatoCounter';
 
 
 const App = () => (
     <Router>
+        {/* <div></div> */}
         <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/'         element={<Home />} />
+            <Route path='/projects/weather'  element={<Weather />} />
+            <Route path='/projects/tomatocounter'  element={<TomatoCounter />} />
+            <Route path='/'         element={<NotFound />} />  
         </Routes>
     </Router>
-    //  <div className='bg-white dark:bg-stone-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter'>
-    //     <div className='max-w-5xl w-11/12 mx-auto'>
-    //         <Intro />
-    //         <Portfolio />
-    //         <Timeline />
-    //         <Contact />
-    //         <Footer />
-    //     </div>
-    // </div>
     
 );
 
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,15 +1,11 @@
 import portfolio from '../data/portfolio'
 import PortfolioItem from './PortfolioItem'
 
-{ /* Loops through portfolio.js to display each project, also passing data fields.
-      Display formatting of the cards including margins is done here as well.
-*/ }
-
 function Portfolio() {
     return(
     <div id="portfolio">
         <h1 className='flex flex-col md:flex-row items-center justify-center text-base md:text-xl mb-6 font-medium'>Portfolio</h1>
-            <div className='flex flex-col md:flex-row md:m-3 items-center justify-center m-3'> 
+            <div className='flex flex-col md:flex-row items-center justify-center'> 
             
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                     {portfolio.map(project => (
