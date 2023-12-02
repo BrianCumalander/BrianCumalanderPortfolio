53578d49e968ba1a0256fb08bd4919491e628bbf
BrianCumalander <koomo24@gmail.com>
2023-12-02 13:42:38 -0700

    Section titles are now the same

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,58 +1,14 @@
 
-
-// function Contact() {
-//     return(
-
-//         <div id="contact">
-//         <h1 className='flex flex-col md:flex-row items-center justify-center text-base md:text-xl mb-6 font-medium'>Contact</h1>
-//             <div className='flex flex-col md:flex-row md:m-3 items-center justify-center m-3'> 
-            
-//                 <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-1'>
-//                 <form 
-//                     action="https://getform.io/f/e1276ff9-2210-4448-93eb-5426d2067fb0"
-//                     method="POST"
-//                     className="flex flex-col w-full md:w-7/12"
-//                 >
-//                     <input 
-//                         type="text"
-//                         name="name"
-//                         placeholder="Name"
-//                         className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
-//                     />
-//                     <input 
-//                         type="text"
-//                         name="email"
-//                         placeholder="Email"
-//                         className="my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none"
-//                     />
-//                     <textarea
-//                     name="message"
-//                     placeholder="Message"
-//                     rows="10"
-//                     className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
-//                     />
-                    
-//                     <button type="button"
-//                     className="hover:stroke-white text-center inline-block px-8 py-3 w-max text-base font-medium
-//                      rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md
-//                       ">Work With Me</button>
-//                     </form>
-//             </div>
-//         </div>
-//     </div>
-//     )
-// }
-
 function Contact() {
     return (
-       <div className="flex flex-col mb-10 mx-auto">
+       <div className="flex flex-col mt-10 mb-10 mx-auto">
           <div className="flex justify-center items-center">
+             
              <form
                 action="https://getform.io/f/e1276ff9-2210-4448-93eb-5426d2067fb0"
                 method="POST"
                 className="flex flex-col w-full md:w-7/12">
-                    
-                <h1>Contact</h1>
+                <h1 className="flex flex-col md:flex-row items-center justify-center text-base md:text-xl mb-6 font-medium">Contact</h1>
                 <input
                    type="text"
                    name="name"
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -7,7 +7,7 @@ function Timeline() {
         <div className='flex flex-col md:flex-row items-center justify-center text-base md:text-xl my-16 mb-6 font-medium'>
                  
             <div className="w-full md:w-7/12">
-                <h1>Earlier Work</h1>
+            <h1 className="flex flex-col md:flex-row items-center justify-center text-base md:text-xl mb-6 font-medium">Earlier Work</h1>
                     {timeline.map(item => (
                         // eslint-disable-next-line react/jsx-key
                         <TimelineItem 
