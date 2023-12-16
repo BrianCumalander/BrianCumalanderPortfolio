333b8ca7ed9ffc40c25d228fef2e4b5bdbdb801a
BrianCumalander <koomo24@gmail.com>
2023-12-16 08:46:12 -0700

    fixed buttons to display based on avalibility of url

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,25 +1,25 @@
 import portfolio from '../data/portfolio'
 import PortfolioItem from './PortfolioItem'
 
-{ /* Loops through portfolio.js to display each project, also passing data fields.
-      Display formatting of the cards including margins is done here as well.
+{ /* Loops through each data key in portfolio.js.
+      Formatting -- The Portfolio card layout is mostly done here, img, title, buttons are formatted in PortfolioItem.jsx
 */ }
 
 function Portfolio() {
-    return(
-    <div id="portfolio">
-        <h1 className='flex flex-col md:flex-row items-center justify-center text-base md:text-xl mb-6 font-medium'>Portfolio</h1>
-            <div className='flex flex-col md:flex-row md:m-3 items-center justify-center m-3'> 
-            
+    return (
+        <div id="portfolio">
+            <h1 className='flex flex-col md:flex-row items-center justify-center text-base md:text-xl mb-6 font-medium'>Portfolio</h1>
+            <div className='flex flex-col md:flex-row md:m-3 items-center justify-center m-3'>
+
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                     {portfolio.map(project => (
                         // eslint-disable-next-line react/jsx-key
                         <PortfolioItem
-                        imgUrl={project.imgUrl}
-                        title={project.title}
-                        git={project.git}
-                        link={project.link}
-                        stack={project.stack}
+                            imgUrl={project.imgUrl}
+                            title={project.title}
+                            git={project.git}
+                            link={project.link}
+                            stack={project.stack}
                         />
                     ))}
                 </div>
@@ -27,5 +27,5 @@ function Portfolio() {
         </div>
     )
 }
- 
+
 export default Portfolio
--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -4,50 +4,66 @@
 
 function PortfolioItem({ title, imgUrl, stack, link, git }) {
 
-    return(
+    // all projects have code on GitHub but not all projects have "live versions" to view.
+    // this code checks for the existance of the live version link. If there is "none", it hides the span that contains the link and button.
 
-    <div className="border-2 border-stone-900 rounded-md overflow-hidden">
+    //     let importedItems = [title, imgUrl, stack, link, git];
 
-    {/* ### Link to live project ### */}
-            <a href={link} target="_blank" rel="noopener noreferrer">
+    // if (importedItems[3].value === "none") {
+    //     document.getElementById("linkSpan").style.display = "none";
+    //     console.log(importedItems[3])
+    // }
 
-    {/* ### Image of live project ### */}
-                <img 
-                    src={imgUrl}
-                    alt="portfolio"
-                    className="w-full h-36 md:h-48 object-cover cursor-pointer"
-                />
-            </a>
+
+
+    return (
+
+        <div className="border-2 border-stone-900 rounded-md overflow-hidden">
+
+            {/* ### Image of live project ### */}
+            <img
+                src={imgUrl}
+                alt="portfolio"
+                className="w-full h-36 md:h-48 object-cover"
+            />
 
             <div className="w-full p-4">
-    {/* ### Project title which also takes you to the live project (link) ### */}
-                <h3 className="text-lg md:text-xl mb-2 md:mb-3 fot-semibold"><a href={link} target="_blank" rel="noreferrer">{title}</a></h3>
+                {/* ### Project title ### */}
+                <h3 className="text-lg md:text-xl mb-2 md:mb-3 fot-semibold">{title}</h3>
 
-                <a href={link} target="_blank" rel="noreferrer">
-                    <button type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center me-2 mb-2">View Live Project</button>
+
+                {/* ### Live Link Button, IF EXISTS ### */}
+                {link !== 'none' && (
+                    <a href={link} target="_blank" rel="noreferrer">
+                        <button type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center me-2 mb-2">View Live Project</button>
                     </a>
+                )}
+
+
+
+                {/* ### GitHub Link Button ### */}
                 <a href={git} target="_blank" rel="noreferrer">
                     <button type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center me-2 mb-2">View Code on GitHub</button>
                 </a>
 
 
 
-                 <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
-    {/* ### tech stack used such as HTML, CSS, JavaScript, loops through the array defined in portfolio.js ### */}
+                <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
+                    {/* ### tech stack used such as HTML, CSS, JavaScript, loops through the array defined in portfolio.js ### */}
                     {stack?.map(item => (
-                    // eslint-disable-next-line react/jsx-key
-                    <span className="inline-block px-2 py-1 font-light border border-stone-900 rounded-md">
-                        {item}
-                    </span>
+                        // eslint-disable-next-line react/jsx-key
+                        <span className="inline-block px-2 py-1 font-light border border-stone-900 rounded-md">
+                            {item}
+                        </span>
                     ))}
                 </p>
-                
+
             </div>
         </div>
 
 
     )
 }
- 
+
 export default PortfolioItem
 
