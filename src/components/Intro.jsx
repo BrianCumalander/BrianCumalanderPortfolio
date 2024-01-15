7af6b1f9357b2242ea9c3cdcd0d515d2b76b9482
BrianCumalander <koomo24@gmail.com>
2024-01-15 08:21:45 -0700

    Added socials to intro. Added link to resume.

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,16 +1,14 @@
-
-
 function Intro() {
-    return(
+    return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
             <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Brian Cumalander</h1>
             <p className="text-base md:text-xl mb-3 font-medium">Full-Stack Developer</p>
 
             <p className="text-sm max-w-xl mb-6 font-bold">
-                
-            
-            I have skills in HTML, CSS, Tailwind, SCSS, JavaScript, TypeScript, Node.JS, React.JS, Vite.js, Express.JS, Recharts.JS, gitHub, mySQL, Hooks, and fetching content from API{"'"}s.
-            
+
+
+                I have skills in HTML, CSS, Tailwind, SCSS, JavaScript, TypeScript, Node.JS, React.JS, Vite.js, Express.JS, Recharts.JS, gitHub, mySQL, Hooks, and fetching content from API{"'"}s.
+                Check out my <span className="underline decoration-blue-700"> <a href="https://github.com/BrianCumalander">GitHub</a></span>, <span className="underline decoration-blue-700"> <a href="https://www.linkedin.com/in/brian-cumalander/">LinkedIn</a></span>, or my <span className="underline decoration-blue-700"> <a href="http://photodub.biz.nf/assets/BrianCumalanderResume.pdf"> Resume</a></span>.
             </p>
         </div>
     )
