0c1be6f3f36dc24ad1dba101209fb3073d93cc7c
Brian Cumalander <koomo24@gmail.com>
2024-02-07 23:29:38 -0700

    Updated about-me description

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,11 +6,8 @@ function Intro() {
             <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Brian Cumalander</h1>
             <p className="text-base md:text-xl mb-3 font-medium">Full-Stack Developer</p>
 
-            <p className="text-sm max-w-xl mb-6 font-bold">
-                
-            
-            I have skills in HTML, CSS, Tailwind, SCSS, JavaScript, TypeScript, Node.JS, React.JS, Vite.js, Express.JS, Recharts.JS, gitHub, mySQL, Hooks, and API content fetching.
-            
+            <p className="text-sm max-w-xl mb-6 font-bold">  
+            Versatile professional with 16+ years of experience as an effective field engineer, demonstrating expertise in problem-solving, meticulous attention to detail, and exceptional customer service. Transitioning to a Software Engineer role with proficiency in HTML, JavaScript, CSS, SCSS, TailwindCSS, Git, GitHub, PHP, TypeScript, Node.js, React.js, Vite.js, Express.js, MySQL, Hooks, and API content fetching.
             </p>
         </div>
     )
