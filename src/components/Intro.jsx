37a6b1bef97169d435926f90e69487be4a6bc2eb
Brian Cumalander <koomo24@gmail.com>
2024-02-07 23:33:18 -0700

    Added 'justify' to about-me

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,7 +6,7 @@ function Intro() {
             <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Brian Cumalander</h1>
             <p className="text-base md:text-xl mb-3 font-medium">Full-Stack Developer</p>
 
-            <p className="text-sm max-w-xl mb-6 font-bold">  
+            <p className="justify-normal text-sm max-w-xl mb-6 font-bold">  
             Versatile professional with 16+ years of experience as an effective field engineer, demonstrating expertise in problem-solving, meticulous attention to detail, and exceptional customer service. Transitioning to a Software Engineer role with proficiency in HTML, JavaScript, CSS, SCSS, TailwindCSS, Git, GitHub, PHP, TypeScript, Node.js, React.js, Vite.js, Express.js, MySQL, Hooks, and API content fetching.
             </p>
         </div>
