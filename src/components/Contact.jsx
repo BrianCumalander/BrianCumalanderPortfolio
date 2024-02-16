684ebf95ca99bb1a33f9499a7635cc91d8f2c0bc
BrianCumalander <koomo24@gmail.com>
2024-02-16 08:25:54 -0700

    update action & submit styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -28,17 +28,12 @@ function Contact() {
                    rows="10"
                    className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
                 />
-                  <button 
-                   type="button" 
-                   className="px-8 py-3 w-max text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center me-2 mb-2">
-                 Work With Me
-                 </button>
-               
+               <button 
+                  type="button" 
+                  className="px-8 py-3 w-max text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center me-2 mb-2">
+                   Work With Me
+               </button>
              </form>
-
-
-
-
           </div>
        </div>
     )
